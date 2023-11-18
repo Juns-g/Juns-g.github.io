5b488f4a6f30a33591328188b2f805975b5a6445
Juns <juns.g@foxmail.com>
2023-11-18 22:20:56 +0800

    Site updated: 2023-11-18 22:20:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/27571/","posts/30980/","posts/19485/","posts/34913/","posts/34307/","posts/62536/","posts/14170/","posts/371/","posts/52847/","posts/39304/","posts/6635/","posts/51463/","posts/43040/","posts/40630/","posts/19697/","posts/17372/","posts/3510/","posts/63534/","posts/61316/","posts/59924/","posts/5855/","posts/31045/","posts/43898/","posts/28536/","posts/33990/"];function toRandomPost(){
+var posts=["posts/34913/","posts/27571/","posts/30980/","posts/19485/","posts/62536/","posts/34307/","posts/14170/","posts/6635/","posts/371/","posts/51463/","posts/39304/","posts/43040/","posts/40630/","posts/19697/","posts/63534/","posts/52847/","posts/3510/","posts/61316/","posts/59924/","posts/17372/","posts/5855/","posts/28536/","posts/43898/","posts/31045/","posts/33990/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
